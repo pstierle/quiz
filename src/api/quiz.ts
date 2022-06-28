b0a7601dc96e895443964bf4d74d1ec199dbdc6a
pstierle <p.stierle@web.de>
2022-06-28 11:06:56 +0200

    removed token

--- src/api/quiz.ts
+++ src/api/quiz.ts
@@ -1,12 +1,9 @@
 import axios from "axios";
 import { IQuestion } from "../model/IQuestion";
 
-const token =
-  "&token=d0f8fd06dcb556eb2d60ae6b304504d97f0169a44265f5ce974772079212374f";
-
 export const fetchQuestions = async () => {
   const { data } = await axios.get<IQuestion[]>(
-    `https://opentdb.com/api.php?amount=5&type=multiple${token}`
+    `https://opentdb.com/api.php?amount=5&type=multiple`
   );
 
   const questions = (data as any).results as IQuestion[];
